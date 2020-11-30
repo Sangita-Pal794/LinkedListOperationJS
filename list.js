52c58ff7d9ea2282f5647262b00db614093f12a3
Sangita-Pal794 <63312258+Sangita-Pal794@users.noreply.github.com>
2020-12-01 00:14:41 +0530

    Added function : To insert last node

--- list.js
+++ list.js
@@ -14,4 +14,21 @@ class singleLinkedList {
         this.head = new Node(data, this.head);
         this.size++;
     }
+    
+    /*to insert last node*/
+    insertNodeLast = (data) => {
+        let node = new Node(data);
+        let present = 0;
+        if (this.head == null) {
+            this.head = node;
+        }
+        else {
+            present = this.head;
+            while (present.next != null) {
+                present = present.next;
+            }
+            present.next = node;
+        }
+        this.size++;
+    }
 }
