19e8eb78f7c000eed9abc325d9ac05750eece1e7
Sangita-Pal794 <63312258+Sangita-Pal794@users.noreply.github.com>
2020-12-01 00:17:12 +0530

    Added function: To insert node at specified index

--- list.js
+++ list.js
@@ -31,4 +31,36 @@ class singleLinkedList {
         }
         this.size++;
     }
+    
+    /*to insert node at specified index*/
+    insertNodeIndex = (data, index) => {
+        if (index < 0 && index > this.size) {
+            console.log("Index Out Of Bound");
+            return;
+        }
+        if (index == 0) {
+            this.insertNodeFirst(data);
+            return;
+        }
+        const node = new Node(data);
+        let present, previous;
+        /*if index is first node*/
+        present = this.head;
+        let size = 0;
+
+        while (size < index && present.next != null) {
+            /*Swaping between previous node & present node*/
+            previous = present;
+            size++;
+            present = present.next;
+        }
+        /*if index is last node*/
+        if (present.next == null) {
+            present.next = node;
+            return;
+        }
+        node.next = present;
+        previous.next = node;
+        this.size++;
+    }
 }
